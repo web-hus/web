f5993af7b24fcfb6542b912c1840c953685c67bd
vietanh03nguyen <vietanh03nguyen@gmail.com>
2024-11-18 13:13:36 +0700

    Revert "Update login.js"
    This reverts commit 11d916384fa488bb4ac6b2c0d971e6e7c5513b6e.

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -13,113 +13,110 @@ const Login = () =>{
 
 
     return(
-        <div className={'functionalContainer'}>
-            <Tabs> 
-                <TabList>
-                    <Tab>ĐĂNG NHẬP</Tab>
-                    <Tab>ĐĂNG KÝ</Tab>
-                </TabList>
+        <div className={'mainContainer'}>
+            <div className={'functionalContainer'}>
+                <Tabs> 
+                    <TabList>
+                        <Tab>ĐĂNG NHẬP</Tab>
+                        <Tab>ĐĂNG KÝ</Tab>
+                    </TabList>
 
-                <TabPanel>
-                    <div className={'mainContainer'}>
+                    <TabPanel>
+                        <div className={'titleContainer'}>
+                            ĐĂNG NHẬP
+                        </div>
                         
-                    </div>
+                        <div className={'inputContainer'}>
+                            <input
+                                value={email}
+                                placeholder="Enter email here..."
+                                onChange={(ev) => setEmail(ev.target.value)}
+                                className={'inputBox'}
+                            />
+                        </div>
 
+                        <br/>
+
+                        <div className={'inputContainer'}>
+                            <input
+                                value={password}
+                                placeholder="Enter Password here..."
+                                onChange={(ev) => setPassword(ev.target.value)}
+                                className={'inputBox'}
+                            />
+                        </div>
+
+                        <br/>
+
+                        <div className={'inputContainer'}>
+                            <input className={'inputButton'} type='button' value={'Log in'}/> 
+                        </div>
+                        <div>
+                            <a href="">Quên mật khẩu?</a>
+                        </div>
+                    </TabPanel>
+                    <TabPanel>
                     <div className={'titleContainer'}>
-                        ĐĂNG NHẬP
-                    </div>
+                        ĐĂNG KÝ
+                        </div>
+
+                        <div className={'inputContainer'}>
+                            <input
+                                value={lastname}
+                                placeholder="Họ"
+                                onChange={(ev) => setLastName(ev.target.value)}
+                                className={'inputBox'}
+                            />
+                        </div>
+
+                        <div className={'inputContainer'}>
+                            <input
+                                value={firstname}
+                                placeholder="Tên"
+                                onChange={(ev) => setFirstName(ev.target.value)}
+                                className={'inputBox'}
+                            />
+                        </div>
+
+
+                        <div className={'inputContainer'}>
+                            <input
+                                value={email}
+                                placeholder="Email"
+                                onChange={(ev) => setEmail(ev.target.value)}
+                                className={'inputBox'}
+                            />
+                        </div>
                     
-                    <div className={'inputContainer'}>
+                        <div className={'inputContainer'}>
                         <input
-                            value={email}
-                            placeholder="Enter email here..."
-                            onChange={(ev) => setEmail(ev.target.value)}
+                            value={phonenum}
+                            placeholder="Số điện thoại"
+                            onChange={(ev) => setPhoneNum(ev.target.value)}
                             className={'inputBox'}
                         />
-                    </div>
-
-                    <br/>
-
-                    <div className={'inputContainer'}>
-                        <input
-                            value={password}
-                            placeholder="Enter Password here..."
-                            onChange={(ev) => setPassword(ev.target.value)}
-                            className={'inputBox'}
-                        />
-                    </div>
-
-                    <br/>
-
-                    <div className={'inputContainer'}>
-                        <input className={'inputButton'} type='button' value={'Log in'}/> 
-                    </div>
-                    <div>
-                        <a href="">Quên mật khẩu?</a>
-                    </div>
-                </TabPanel>
-                <TabPanel>
-                <div className={'titleContainer'}>
-                    ĐĂNG KÝ
-                    </div>
-
-                    <div className={'inputContainer'}>
-                        <input
-                            value={lastname}
-                            placeholder="Họ"
-                            onChange={(ev) => setLastName(ev.target.value)}
-                            className={'inputBox'}
-                        />
-                    </div>
-
-                    <div className={'inputContainer'}>
-                        <input
-                            value={firstname}
-                            placeholder="Tên"
-                            onChange={(ev) => setFirstName(ev.target.value)}
-                            className={'inputBox'}
-                        />
-                    </div>
+                        </div>
 
 
-                    <div className={'inputContainer'}>
-                        <input
-                            value={email}
-                            placeholder="Email"
-                            onChange={(ev) => setEmail(ev.target.value)}
-                            className={'inputBox'}
-                        />
-                    </div>
-                
-                    <div className={'inputContainer'}>
-                    <input
-                        value={phonenum}
-                        placeholder="Số điện thoại"
-                        onChange={(ev) => setPhoneNum(ev.target.value)}
-                        className={'inputBox'}
-                    />
-                    </div>
+                        <div className={'inputContainer'}>
+                            <input
+                                value={password}
+                                placeholder="New Password"
+                                onChange={(ev) => setPassword(ev.target.value)}
+                                className={'inputBox'}
+                            />
+                        </div>
 
+                        <br/>
 
-                    <div className={'inputContainer'}>
-                        <input
-                            value={password}
-                            placeholder="New Password"
-                            onChange={(ev) => setPassword(ev.target.value)}
-                            className={'inputBox'}
-                        />
-                    </div>
-
-                    <br/>
-
-                    <div className={'inputContainer'}>
-                        <input className={'inputButton'} type='button' value={'Sign Up'}/> 
-                    </div>
-                </TabPanel>
-            </Tabs>
+                        <div className={'inputContainer'}>
+                            <input className={'inputButton'} type='button' value={'Sign Up'}/> 
+                        </div>
+                    </TabPanel>
+                </Tabs>
+            </div>
+            
         </div>
-        
-        
             
     )
 }
