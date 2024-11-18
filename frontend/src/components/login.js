359b09d12d97ece3b41af6c7ba66db5cba011942
vietanh03nguyen <vietanh03nguyen@gmail.com>
2024-11-18 19:44:48 +0700

    Update login.js

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -3,8 +3,10 @@ import './login.css'
 import {Tab, Tabs, TabList, TabPanel} from 'react-tabs'
 
 const Login = () =>{
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [loginEmail, setLoginEmail] = useState('')
+    const [loginPassword, setLoginPassword] = useState('')
+    const [signupPassword, setSignupPassword] = useState('')
+    const [signupEmail, setSignupEmail] = useState('')
     const [lastname, setLastName] = useState('')
     const [firstname, setFirstName] = useState('')
     const [phonenum, setPhoneNum] = useState('')
@@ -28,9 +30,9 @@ const Login = () =>{
                         
                         <div className={'inputContainer'}>
                             <input
-                                value={email}
+                                value={loginEmail}
                                 placeholder="Enter email here..."
-                                onChange={(ev) => setEmail(ev.target.value)}
+                                onChange={(ev) => setLoginEmail(ev.target.value)}
                                 className={'inputBox'}
                             />
                         </div>
@@ -39,9 +41,9 @@ const Login = () =>{
 
                         <div className={'inputContainer'}>
                             <input
-                                value={password}
+                                value={loginPassword}
                                 placeholder="Enter Password here..."
-                                onChange={(ev) => setPassword(ev.target.value)}
+                                onChange={(ev) => setLoginPassword(ev.target.value)}
                                 className={'inputBox'}
                             />
                         </div>
@@ -52,11 +54,24 @@ const Login = () =>{
                             <input className={'inputButton'} type='button' value={'Log in'}/> 
                         </div>
                         <div>
+<<<<<<< Updated upstream
                             <a href="">Quên mật khẩu?</a>
                         </div>
+=======
+                            <a className='text' href="">Quên mật khẩu?</a>
+                            <div className='text'>Hoặc đăng nhập bằng</div>
+                            <br/>
+                            <div className='text'>
+                                <button className='facebook'>Facebook</button>
+                                <button className='google'>Google</button>
+                            </div>
+                                
+                        </div>
+                        <br/>
+>>>>>>> Stashed changes
                     </TabPanel>
                     <TabPanel>
-                    <div className={'titleContainer'}>
+                        <div className={'titleContainer'}>
                         ĐĂNG KÝ
                         </div>
 
@@ -81,9 +96,9 @@ const Login = () =>{
 
                         <div className={'inputContainer'}>
                             <input
-                                value={email}
+                                value={signupEmail}
                                 placeholder="Email"
-                                onChange={(ev) => setEmail(ev.target.value)}
+                                onChange={(ev) => setSignupEmail(ev.target.value)}
                                 className={'inputBox'}
                             />
                         </div>
@@ -100,9 +115,9 @@ const Login = () =>{
 
                         <div className={'inputContainer'}>
                             <input
-                                value={password}
+                                value={signupPassword}
                                 placeholder="New Password"
-                                onChange={(ev) => setPassword(ev.target.value)}
+                                onChange={(ev) => setSignupPassword(ev.target.value)}
                                 className={'inputBox'}
                             />
                         </div>
