8452e3e7df044b9f846b2cb23f7f42878bfaf0b4
phanhn438@gmail.com <phanhn438@gmail.com>
2024-12-14 20:42:02 +0700

    phanh with blackpink

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import BannerImage from "../assets/pizza.jpeg";
 import "../styles/Home.css";
+import bannerVideo from "../assets/Now_in_your_area.mp4";
 
 function Home() {
   return (
-    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+    <div className="home">
+      <video className="background-video" autoPlay loop muted>
+        <source src={bannerVideo} type="video/mp4" />
+      </video>
       <div className="headerContainer">
-        <h1> Pizza Phe La</h1>
-        <p> PIZZA TO FIT ANY TASTE</p>
+        <h1>Pizza Phe La</h1>
+        <p>PIZZA TO FIT ANY TASTE</p>
         <Link to="/menu">
-          <button> ORDER NOW </button>
+          <button>ORDER NOW</button>
         </Link>
       </div>
     </div>
