c4fe0687a382615365c2d13805f9978ea5a1b1a0
phanhn438@gmail.com <phanhn438@gmail.com>
2025-01-17 19:17:11 +0700

    phanh edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,10 +17,13 @@ import FoodDes from "./pages/Food_Des";
 import Cart from "./pages/Cart";
 import LostPassword from "./pages/Lost_Password";
 import NewPassword from "./pages/New_Password";
-import Home_admin from "./admin/pages/Home_admin"; 
+import Home_admin from "./admin/pages/Home_admin";
 import Menu_managment from "./admin/pages/Menu_managment"
 import User_managment from "./admin/pages/User_managment"
 import Dashboard from "./admin/pages/Dashboard"
+import Booking_managment from "./admin/pages/Booking_managment"
+import Order_managment from "./admin/pages/Order_managment"
+import Booking_order from "./admin/pages/Booking_order"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
@@ -60,7 +63,7 @@ function App() {
     alert("Không có quyền truy cập!");
     return <Navigate to="/" replace />;
   };
-  
+
 
   return (
     <div className="App">
@@ -76,8 +79,8 @@ function App() {
           <Route path="/news" exact element={<News />} />
           <Route path="/food/:id" exact element={<FoodDes />} />
 
-          
-{/* 
+
+          {/* 
           <Route 
             path="/Home_admin" 
             element={isAdmin ? <Home_admin /> : <NotAuthorizedHandler />} 
@@ -93,8 +96,14 @@ function App() {
           <Route path="/Menu_managment" exact element={<Menu_managment />} />
           <Route path="/User_managment" exact element={<User_managment />} />
           <Route path="/Dashboard" exact element={<Dashboard />} />
+          {/* <Route path="/Test" element={<UserProfile />} /> */}
+          <Route path="/Order_managment" element={<Order_managment />}
+          />
+          <Route path="/Booking_managment" element={<Booking_managment />}
+          />
+          <Route path="/Booking_order" element={<Booking_order />}
+          />
 
-          <Route path="/Test" element={<UserProfile />} />
           <Route path="/Payment" element={<Payment />} />
           <Route path="/Cart" element={<Cart />} />
           <Route path="/LostPassword" exact element={<LostPassword />} />
@@ -120,6 +129,14 @@ function App() {
           <Route
             path="/Order_managment"
             element={isAdmin ? <Order_managment /> : <NotAuthorizedHandler />}
+          />
+          <Route
+            path="/Booking_managment"
+            element={isAdmin ? <Booking_managment /> : <NotAuthorizedHandler />}
+          />
+          <Route
+            path="/Booking_order"
+            element={isAdmin ? <Booking_order /> : <NotAuthorizedHandler />}
           />
 
           {/* Catch-All Route for Unknown Paths */}
