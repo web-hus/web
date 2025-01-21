a6a8cdf434647160895e8ea14d8732aef9a4f51f
VietAnh <vietanh03nguyen@gmail.com>
2025-01-21 17:55:10 +0700

    Thay đổi route của API password: Bỏ prefix trong main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -47,7 +47,7 @@ app.include_router(profile_router, prefix="/api/profile", tags=["profile"])
 app.include_router(cart_router, prefix="/api/cart", tags=["cart"])
 app.include_router(dish_router, prefix="/api/dish", tags=["dish"])
 app.include_router(menu_router, prefix="/api/menu", tags=["menu"])
-app.include_router(password_router, prefix="/api/password", tags=["password"])
+app.include_router(password_router, tags=["password"])
 app.include_router(registration_router, tags=["registration"])
      
 
